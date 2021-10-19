3e4e27b8e90034f52022bfdc54ab813907ac8df7
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-19 12:32:09 +0200

    send request with axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+const axios = require("axios");
+
+const nickname =  document.querySelector('#nickname').value;
+const message = document.querySelector('#message').value;
+const submitButton = document.querySelector('#submitButton');
+
+submitButton.addEventListener('click', () => {
+    axios.post('/chat', {
+        nickname: nickname,
+        message: message
+    });
+});
